56d40cb92882230453e51006f2395779bf0445b0
MrRahmat <vovacet12@mail.ru>
2022-02-12 14:09:33 +0300

    Add the component for weather data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { fetchWeather } from './api/fetchWeather';
 
+import City from "./components/City.jsx";
 import './App.css';
 
 const App = () => {
@@ -25,24 +26,7 @@ const App = () => {
             onKeyPress={search}
         />
         { weather.main && (
-            <div className='city'>
-                <h2 className='city-name'>
-                    <span>{weather.name}</span>
-                    <sup>{weather.sys.country}</sup>
-                </h2>
-                <div className='city-temp'>
-                    {Math.round(weather.main.temp)}
-                    <sup>&deg;C</sup>
-                </div>
-                <div className='info'>
-                    <img 
-                        className='city-icon'
-                        src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-                        alt={weather.weather[0].description}
-                    />
-                    <p>{weather.weather[0].description}</p>
-                </div>
-            </div>
+            <City data={weather}/>
         )}
     </div>
   )
---
+++ src/components/City.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const City = ({ data }) => {
+  return (
+    <div className='city'>
+        <h2 className='city-name'>
+            <span>{data.name}</span>
+            <sup>{data.sys.country}</sup>
+        </h2>
+        <div className='city-temp'>
+            {Math.round(data.main.temp)}
+            <sup>&deg;C</sup>
+        </div>
+        <div className='info'>
+            <img 
+                className='city-icon'
+                src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+                alt={data.weather[0].description}
+            />
+            <p>{data.weather[0].description}</p>
+        </div>
+    </div>
+  )
+}
+
+export default City;
