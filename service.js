edab0b356d499a6e1834266d0378f4765a43cc45
MrRahmat <vovacet12@mail.ru>
2022-02-14 07:53:10 +0300

    Updates

--- service.js
+++ service.js
@@ -2,20 +2,37 @@ const CACHE_NAME = "version-1";
 const urls = [ 'index.html', 'offline.html' ];
 const self = this;
 
-self.addEventListener('install', ( event ) => {
+self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-        .then((cache) => {
-            console.log("Opened cache");
-            return cache.addAll(urls);
-        })
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(urls);
+            })
     )
 });
 
 self.addEventListener('fetch', ( event ) => {
-
+    event.respondWith(
+        caches.match(event.request)
+        .then(() => {
+            return fetch(event.request)
+            .catch(() => caches.match('offline.html'))
+        })
+    )
 });
 
 self.addEventListener('activate', ( event ) => {
+    const cacheWhiteList = [];
+    cacheWhiteList.push(CACHE_NAME);
 
+    event.waitUntil(
+        caches.keys().then((names) => Promise.all(
+            names.map((name) => {
+                if(!cacheWhiteList.includes(name)) {
+                    return caches.delete(name);
+                }
+            })
+        ))
+    )
 });
